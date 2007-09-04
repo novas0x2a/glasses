1333726ea6d308e0dbcea2527087626e6b6202e7
Mike Lundy <mike@fluffypenguin.org>
2007-09-03 18:25:11 -0700

    can set/get params now

--- grab.cc
+++ grab.cc
@@ -14,44 +14,37 @@
 
 using namespace std;
 
-#if 0
-#include <assert.h>
-
-#endif
-#if 0
-char _pdie[255] = {0};
-char ret = 0;
-
-#define pwarn(...) do                       \
-{                                           \
-    snprintf(_pdie, 254, __VA_ARGS__);      \
-    perror(_pdie);                          \
-} while (0)
-
-#define pgoto(lbl, ...) do                  \
-{                                           \
-    pwarn(__VA_ARGS__);                     \
-    ret = 1;                                \
-    goto lbl;                               \
-} while (0)
-
-typedef struct {
-    uint16_t depth;
-    uint16_t palette;
-} CapMode;
-
-CapMode modes[] = {
-    {24, VIDEO_PALETTE_RGB24},
-    {16, VIDEO_PALETTE_RGB565},
-    {15, VIDEO_PALETTE_RGB555}
-};
-
-const unsigned mode_count = sizeof(modes) / sizeof(modes[0]);
-
-#endif
 typedef struct video_capability VideoCapability;
 typedef struct video_window     VideoWindow;
 typedef struct video_picture    VideoPicture;
+
+ostream& operator<< (ostream& os, VideoCapability& a) {
+    os  << "Name["        << a.name
+        << "] Type["      << a.type
+        << "] Channels["  << a.channels
+        << "] Audios["    << a.audios
+        << "] MaxWidth["  << a.maxwidth
+        << "] MaxHeight[" << a.maxheight
+        << "] MinWidth["  << a.minwidth
+        << "] MinHeight["  << a.minheight << "]";
+    return os;
+}
+
+ostream& operator<< (ostream& os, VideoWindow& a) {
+    os  << "Pos[" << a.x << "," << a.y << "] Size[" << a.width << "," << a.height << "]";
+    return os;
+}
+
+ostream& operator<< (ostream& os, VideoPicture& a) {
+    os  << "Brightness["    << a.brightness
+        << "] Hue["         << a.hue
+        << "] Color["       << a.colour
+        << "] Contrast["    << a.contrast
+        << "] Whiteness["   << a.whiteness
+        << "] Depth["       << a.depth
+        << "] Palette["     << a.palette << "]";
+    return os;
+}
 
 class VideoDevice
 {
@@ -60,12 +53,15 @@ class VideoDevice
         VideoDevice(const char *);
         ~VideoDevice(void);
 
-        VideoCapability getCap(void);
-        VideoWindow     getWin(void);
-        VideoPicture    getPic(void);
+        VideoCapability& getCap(void);
+        VideoWindow&     getWin(void);
+        VideoPicture&    getPic(void);
 
-        void setWin(const VideoWindow&    );
-        void setPic(const VideoPicture&   );
+        void setWin(const VideoWindow&);
+        void setPic(const VideoPicture&);
+
+        void setParams(const uint32_t width, const uint32_t height, const uint16_t depth = 0);
+        pair<pair<uint32_t, uint32_t>, uint16_t> getParams(void);
 
     private:
         const string devname;
@@ -76,7 +72,7 @@ VideoDevice::VideoDevice(const char *device) : devname(device)
 {
     VideoCapability cap;
 
-    if ((dev = open(device, O_RDONLY)) < 0)
+    if ((dev = open(devname.c_str(), O_RDONLY)) < 0)
         throw string("Could not open video device");
 
     if (ioctl(dev, VIDIOCGCAP, &cap) < 0)
@@ -90,22 +86,76 @@ VideoDevice::~VideoDevice(void)
 }
 
 
-VideoCapability VideoDevice::getCap(void)
+VideoCapability& VideoDevice::getCap(void)
 {
+    static VideoCapability cap;
+    if (ioctl(dev, VIDIOCGCAP, &cap) < 0)
+        throw string("Couldn't get capabilities");
+    return cap;
 }
 
-VideoWindow VideoDevice::getWin(void)
+VideoWindow& VideoDevice::getWin(void)
 {
+    static VideoWindow win;
+    if (ioctl(dev, VIDIOCGWIN, &win) < 0)
+        throw string("Couldn't get window");
+    return win;
 }
 
-VideoPicture VideoDevice::getPic(void)
+VideoPicture& VideoDevice::getPic(void)
 {
+    static VideoPicture vpic;
+    if (ioctl(dev, VIDIOCGPICT, &vpic) < 0)
+        throw string("Couldn't get picture");
+    return vpic;
 }
 
+void VideoDevice::setWin(const VideoWindow& win)
+{
+    if(ioctl(dev, VIDIOCSWIN, &win) < 0)
+        throw string("Couldn't set window");
+}
+
+void VideoDevice::setPic(const VideoPicture& pic)
+{
+    if(ioctl(dev, VIDIOCSPICT, &pic) < 0)
+        throw string("Couldn't set picture");
+}
+
+void VideoDevice::setParams(const uint32_t width, const uint32_t height, const uint16_t depth)
+{
+    VideoWindow w = this->getWin();
+    w.width  = width;
+    w.height = height;
+    this->setWin(w);
+
+    if (depth != 0)
+    {
+        VideoPicture p = this->getPic();
+        p.depth = depth;
+        this->setPic(p);
+    }
+}
+
+pair<pair<uint32_t, uint32_t>, uint16_t> VideoDevice::getParams(void)
+{
+    static pair<pair<uint32_t, uint32_t>, uint16_t> data;
+    VideoWindow  w = this->getWin();
+    VideoPicture p = this->getPic();
+
+    data.first.first = w.width;
+    data.first.second = w.height;
+    data.second = p.depth;
+
+    return data;
+}
 
 void Run(void)
 {
     VideoDevice v("/dev/video0");
+
+    v.setParams(640,480,24);
+    cout << v.getCap() << endl << v.getWin() << endl << v.getPic() << endl;
 }
 
 int main(int argc, char *argv[])
@@ -119,34 +169,6 @@ int main(int argc, char *argv[])
 }
 
 #if 0
-    int v;
-    unsigned i;
-    unsigned char *buffer, *src;
-
-    if ((v = open("/dev/video0", O_RDONLY)) < 0)
-        pgoto(exit0, "Could not open video device");
-
-    sleep(5);
-
-    if (ioctl(v, VIDIOCGCAP, &cap) < 0)
-        pgoto(exit1, "Couldn't get capabilities. Not a V4L device?\n");
-
-    if (ioctl(v, VIDIOCGWIN, &win) < 0)
-        pgoto(exit1, "Couldn't get window\n");
-
-    if (ioctl(v, VIDIOCGPICT, &vpic) < 0)
-        pgoto(exit1, "Couldn't get picture\n");
-
-    for (i = 0; i < mode_count; ++i)
-    {
-        vpic.palette = modes[i].palette;
-        vpic.depth   = modes[i].depth;
-        if(ioctl(v, VIDIOCSPICT, &vpic) >= 0)
-            goto got_one;
-    }
-    pgoto(exit1, "Couldn't pick a good video mode");
-
-got_one:
 
     buffer = malloc(win.width * win.height * vpic.depth);
     read(v, buffer, win.width * win.height * vpic.depth);
