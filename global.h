39ce5fd4c591082dcb745aa660e37943c55054c8
Mike Lundy <mike@fluffypenguin.org>
2007-09-22 23:44:40 -0700

    added a value accessor

--- global.h
+++ global.h
@@ -3,6 +3,7 @@
 
 #include <string>
 #include <sstream>
+#include <cmath>
 
 #define likely(x)       __builtin_expect((x),1)
 #define unlikely(x)     __builtin_expect((x),0)
@@ -10,10 +11,11 @@
 typedef unsigned char byte;
 typedef byte Pixel __attribute__((vector_size (4)));
 
-inline byte& R(const Pixel &p) {return ((byte*)&p)[2];}
-inline byte& G(const Pixel &p) {return ((byte*)&p)[1];}
-inline byte& B(const Pixel &p) {return ((byte*)&p)[0];}
-inline byte& A(const Pixel &p) {return ((byte*)&p)[3];}
+inline byte&  R(const Pixel &p) {return ((byte*)&p)[2];}
+inline byte&  G(const Pixel &p) {return ((byte*)&p)[1];}
+inline byte&  B(const Pixel &p) {return ((byte*)&p)[0];}
+inline byte&  A(const Pixel &p) {return ((byte*)&p)[3];}
+inline double V(const Pixel &p) {return sqrt(pow((double)R((p)),2) + pow((double)G((p)),2) + pow((double)B((p)),2));}
 
 inline Pixel RGB(byte r, byte g, byte b, byte a = 0) {return (Pixel){b,g,r,a};}
 
