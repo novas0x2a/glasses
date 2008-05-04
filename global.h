0326b4c491f72056d8661f8b555dfd28aaded3e3
Mike Lundy <mike@fluffypenguin.org>
2008-05-04 02:44:30 -0700

    fix a stupid divide-by-zero trap

--- global.h
+++ global.h
@@ -17,7 +17,7 @@ inline byte&  B(const Pixel &p) {return ((byte*)&p)[0];}
 inline byte&  A(const Pixel &p) {return ((byte*)&p)[3];}
 inline double V(const Pixel &p) {return 0.2989*R(p) + 0.5866*G(p) + 0.1145*B(p);}
 
-inline Pixel RGB(byte r, byte g, byte b, byte a = 0) {return (Pixel){b,g,r,a};}
+inline Pixel RGB(byte r, byte g, byte b, byte a = 1) {return (Pixel){b,g,r,a};}
 
 template<typename T>
 inline std::string stringify(const T& x)
